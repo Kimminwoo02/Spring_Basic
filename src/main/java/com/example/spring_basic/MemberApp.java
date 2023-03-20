cbc9788f0e673f580fedf1159a41992a330b72df
minu <made_power@naver.com>
2023-03-20 14:39:01 +0900

    스프링 컨테이너 등록
    기존 자바코드만 이용해서 DI컨테이너를 사용했지만 
    스프링을 이용하여 @Bean을 스프링 컨테이너에 등록한 후 사용하도록 변경

--- src/main/java/com/example/spring_basic/MemberApp.java
+++ src/main/java/com/example/spring_basic/MemberApp.java
@@ -4,11 +4,16 @@ import com.example.spring_basic.member.Grade;
 import com.example.spring_basic.member.Member;
 import com.example.spring_basic.member.MemberService;
 import com.example.spring_basic.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+        //AppConfig appConfig = new AppConfig();
+        //MemberService memberService = appConfig.memberService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
 
         Member member = new Member(1L, "MemberA", Grade.VIP);
         memberService.join(member);
--- src/main/java/com/example/spring_basic/OrderApp.java
+++ src/main/java/com/example/spring_basic/OrderApp.java
@@ -2,15 +2,23 @@ package com.example.spring_basic;
 
 import com.example.spring_basic.member.Grade;
 import com.example.spring_basic.member.Member;
+
 import com.example.spring_basic.member.MemberService;
 import com.example.spring_basic.order.Order;
 import com.example.spring_basic.order.OrderService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 
 public class OrderApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
 
         Long memberId = 1L;
